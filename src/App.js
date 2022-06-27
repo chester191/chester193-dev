c77167028cf84131b8bb7b55a7a39e335b8a10de
chester-kalevamedia <chester@kalevamedia.com>
2022-06-27 10:06:45 +0200

    migrated to react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-
+// import Topbar from "/components/topbar/Topbar";
+import Topbar from "./components/topbar/Topbar.jsx";
+import Intro from "./components/intro/Intro.jsx";
+// import Portfolio from "./components/portfolio/Portfolio"
+import Portfolio from "./components/Portfolio/Portfolio.jsx";
+// import Works from "./components/works/Works.jsx";
+import Testimonials from "./components/testimonials/Testimonials.jsx";
+// import Contact from "./components/contact/Contact";
+import Contact from "./components/Contact/Contact.jsx";
+import "./app.scss";
+import { useState } from "react";
+import Menu from "./components/menu/Menu.jsx";
+// import Menu from "./components/menu/Menu";
+// import Menu from "./components/men"
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
-    <div className="App">
-      Hello
-    </div>
+      <div className="app">
+          <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+          <div className="sections">
+            <Intro/>
+            <Portfolio/>
+            {/* <Works/> */}
+            <Testimonials/>
+            <Contact/>
+          </div>
+      </div>
   );
 }
 
